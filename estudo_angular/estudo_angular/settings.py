2c2ecb8e1f05416a7cb568c3ddfd09f4c0701b86
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-08-13 20:45:06 -0300

    adição dos apps criados e rest_framework

--- estudo_angular/estudo_angular/settings.py
+++ estudo_angular/estudo_angular/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
+    'corsheaders',
+    'home.apps.HomeConfig',
+    'EmployeeApp.apps.EmployeeappConfig',
+    'rest_framework',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
