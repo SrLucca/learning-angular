43010201380ff4941d19c213e397400a7169fb52
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-08-15 23:13:21 -0300

    url correpondente as medias

--- estudo_angular/EmployeeApp/urls.py
+++ estudo_angular/EmployeeApp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from django.urls import include, re_path
 from EmployeeApp import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 urlpatterns = [
     #path(),
     re_path(r'^departament/$', views.departamentApi),
@@ -10,4 +14,6 @@ urlpatterns = [
     re_path(r'^employees/$', views.employeesApi),
     re_path(r'^employees/([0-9]+)$', views.employeesApi),
 
-]
+    re_path(r'^SaveFile$', views.saveFile),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
