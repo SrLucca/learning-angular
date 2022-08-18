cf81f1c1ec610f4c711e38af15436ab31b1dc214
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-08-18 15:55:35 -0300

    add api methods and components

--- ui/studyingangular/src/app/app.module.ts
+++ ui/studyingangular/src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartamentComponent } from './departament/departament.component';
+import { ShowDepComponent } from './departament/show-dep/show-dep.component';
+import { AddEditDepComponent } from './departament/add-edit-dep/add-edit-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
+import { SharedService } from './shared.service';
+
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartamentComponent,
+    ShowDepComponent,
+    AddEditDepComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
