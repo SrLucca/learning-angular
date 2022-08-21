3bf889ebe86e209751e554d5416b494cdfa899d6
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-08-20 23:02:24 -0300

    functions of dep

--- ui/studyingangular/src/app/departament/show-dep/show-dep.component.ts
+++ ui/studyingangular/src/app/departament/show-dep/show-dep.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-dep',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowDepComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  DepartamentList:any = [];
+
+
+  ModalTitle:string;
+  ActivateAddEditDepComp:boolean=false;
+  dep:any;
 
   ngOnInit(): void {
+    this.refreshDepList();
   }
 
+  addClick(){
+    this.dep={
+      DepartamentId:0,
+      DepartamentName:""
+    }
+    this.ModalTitle = "Add Departament";
+    this.ActivateAddEditDepComp = true;
+  }
+
+
+  refreshDepList(){
+    this.service.getDepList().subscribe(data=>{
+      this.DepartamentList=data;
+    });
+  }
+  
 }
