4ebe435f36201efc010c773a376fbe465e629a80
SrLucca <luccatsoares@gmail.com>
2022-09-18 18:29:43 -0300

    routes to access apps components

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarsComponent } from './cars/cars.component';
+import { ShowCarsComponent } from './cars/show-cars/show-cars.component';
+import { CrudCarsComponent } from './cars/crud-cars/crud-cars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cars', component: CarsComponent},
+  {path:'view-cars-on-sale', component: ShowCarsComponent},
+  {path:'sale-car', component: CrudCarsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
