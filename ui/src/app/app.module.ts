f99f5a043ce18d8e159c9327f033894fc2440fc1
SrLucca <luccatsoares@gmail.com>
2022-09-18 18:29:49 -0300

    Update app.module.ts

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { CrudCarsComponent } from './cars/crud-cars/crud-cars.component';
+import { ShowCarsComponent } from './cars/show-cars/show-cars.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CrudCarsComponent,
+    ShowCarsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
