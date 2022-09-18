415dd751284755ecb77ae751dd970065e1038268
SrLucca <luccatsoares@gmail.com>
2022-09-18 18:30:17 -0300

    Create crud-cars.component.ts

---
+++ ui/src/app/cars/crud-cars/crud-cars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-cars',
+  templateUrl: './crud-cars.component.html',
+  styleUrls: ['./crud-cars.component.css']
+})
+export class CrudCarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
